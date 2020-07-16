48150b1b6ed6007ec00f2e52991da169047ab0a5
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-07-16 19:16:32 +0300

    Added new file for work with stack use function in NASM

---
+++ BeginProg/MyFirstFunction.asm
@@ -0,0 +1,21 @@
+global _start
+
+
+section .bss
+my_array	resb	256
+
+section .text
+
+; fil memory (edi=address, ecx=length, al=value)
+fill_memory:
+		jecxz	fm_q
+fm_lp:		mov	[edi], al
+		inc	edi
+		loop	fm_lp
+fm_q:		ret
+;main program
+_start:
+		mov	edi, my_array
+		mov	ecx, 256
+		mov	al, '@'
+		call	fill_memory
